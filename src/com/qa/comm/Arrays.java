0cead97644194c562043162e2703062318edcdd8
Imran-Subhan <isubhan@qa.com>
2020-06-25 17:36:05 +0100

    Comminity exercise

---
+++ src/com/qa/comm/Arrays.java
@@ -0,0 +1,14 @@
+package com.qa.comm;
+
+public class Arrays {
+
+	public static int array[] = {0,1,2,3,4,5,6,7,8,9};
+	
+	public static void main(String[] args) {
+		
+		for (int i : array) {
+			
+			System.out.println("Number:" + i); }
+	}
+
+}
