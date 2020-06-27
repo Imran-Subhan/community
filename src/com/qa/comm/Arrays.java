b9e8880241f00708884559c5eef4599cecd1987c
Imran-Subhan <isubhan@qa.com>
2020-06-27 18:44:10 +0100

    exercise

--- src/com/qa/comm/Arrays.java
+++ src/com/qa/comm/Arrays.java
@@ -1,14 +1,16 @@
-package com.qa.comm;
-
-public class Arrays {
-
-	public static int array[] = {0,1,2,3,4,5,6,7,8,9};
-	
-	public static void main(String[] args) {
-		
-		for (int i : array) {
-			
-			System.out.println("Number:" + i); }
-	}
-
-}
+//package com.qa.comm;
+//
+//public class Arrays {
+//
+//	public static int array[] = {0,1,2,3,4,5,6,7,8,9};
+//	
+//	public static void main(String[] args) {
+//		
+//		for (int i : array) {
+//			
+//			System.out.println("Number:" + i); }
+//	}
+//
+//}
+//
+//In.next
