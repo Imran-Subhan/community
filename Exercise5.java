7a15d348c3beb2fa1c574e02efe5efa2668da94e
Imran-Subhan <isubhan@qa.com>
2020-06-27 18:52:27 +0100

    Exercise5 added

--- Exercise5.java
+++ Exercise5.java
@@ -1,37 +1,3 @@
-package Java1;
 
-public class Exercise5 {
-	
-
-
-
-	public static void execise5(String[] args) {
-		
-		
-		String[] units = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-		
-//		String[] decades = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-		
-//		string[] hundreds =
-		
-//		for (String unit : units) {
-//		
-//			System.out.println(unit);
-//		}
-		
-		for (int i = 0; i < 100; i++) {
-		
-		if( i < 20); {
-			
-			System.out.println(units[i]); 
-
-//		} (i > 40) {
-//				System.out.println(decades[i/10] + units[i%10]);
-		
-		}
-		
-	}
-}
-}
 
 
